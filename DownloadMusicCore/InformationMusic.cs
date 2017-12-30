83844a41d858c50b888c266edbc79c7e8aeef2f5
Serios7 <Serios601@gmail.com>
2017-12-30 10:14:47 +1000

    INIT

---
+++ DownloadMusicCore/InformationMusic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DownloadMusicCore
+{
+    public class InformationMusic
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Quality { get; set; }
+        public string Duration { get; set; }
+        public string Link { get; set; }
+    }
+}
