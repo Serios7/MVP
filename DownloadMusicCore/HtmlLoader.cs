b8f78c28a5325ae0a2b0296ae74cffb9b52a8901
Serios7 <Serios601@gmail.com>
2017-12-30 20:43:27 +1000

    1

---
+++ DownloadMusicCore/HtmlLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace DownloadMusicCore
+{
+    class HtmlLoader
+    {
+        readonly WebClient webClient;
+        readonly string url;
+
+        public HtmlLoader(IParserSettings settings)
+        {
+            webClient = new WebClient()
+            {
+                Encoding = Encoding.UTF8
+            };
+            url = $"{settings.BaseUrl}{settings.Search}{settings.PageNumber}";
+        }
+
+        public async Task<string> GetSourceBySearch(string search, int pageNumber)
+        {
+            var currentUrl = url.Replace("{search}", search).Replace("{pageNumber}", pageNumber.ToString());
+            return await webClient.DownloadStringTaskAsync(currentUrl);
+        }
+    }
+}
---
+++ DownloadMusicCore/IParserSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace DownloadMusicCore
+{
+    interface IParserSettings
+    {
+        string BaseUrl { get; set; }
+
+        string Search { get; set; }
+
+        int PageNumber { get; set; }
+    }
+}
