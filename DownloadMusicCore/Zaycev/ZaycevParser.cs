1e6889acf726ddf59c76d076e96d72c4783a17da
Serios7 <Serios601@gmail.com>
2018-01-04 11:37:11 +1000

    init

--- DownloadMusicCore/Zaycev/ZaycevParser.cs
+++ DownloadMusicCore/Zaycev/ZaycevParser.cs
@@ -11,9 +11,17 @@ namespace DownloadMusicCore.Zaycev
     class ZaycevParser : IParser<List<InformationMusic>>
     {
         public event Action OnNotFound;
+        readonly Regex regex = new Regex(@"длительность([^\t]+)размер([^\t]+)битрейт([^\t]+)Скачать");
 
         List<InformationMusic> IParser<List<InformationMusic>>.Parse(IHtmlDocument document)
-        {
+        {            
+            List<InformationMusic> items = new List<InformationMusic>();
+            if (IsNotFound(document))
+            {
+                OnNotFound?.Invoke();
+                return items;
+            }
+
             var itemsBlock = document.QuerySelectorAll("div.zaycev__block");            
             var itemsTitle = itemsBlock.Select(s => s.GetElementsByClassName("light-link"));
             var itemsRecord = itemsBlock.Select(s => s.GetElementsByClassName("result__snp"));
@@ -21,10 +29,29 @@ namespace DownloadMusicCore.Zaycev
                                       .Select(m => m.GetAttribute("href")));
 
 
+            for (int i = 0; i < itemsBlock.Length; i++)
+            {
+                var record = regex.Split(itemsRecord.ElementAt(i).First().TextContent.Replace("\n", "").Replace(" ", ""));
+                var title = itemsTitle.ElementAt(i).First().TextContent.Split(new string[] { " - " }, StringSplitOptions.RemoveEmptyEntries);
 
+                items.AddRange(new InformationMusic[] { new InformationMusic()
+                {
+                    Author = title.ElementAt(0),
+                    Name = title.ElementAt(1),
+                    Duration = record.ElementAt(1),
+                    Size = record.ElementAt(2),
+                    Quality = record.ElementAt(3),                 
+                    Link = itemsLink.ElementAt(i).First()
+                }});
+            }
 
+            return items;
+        }
 
-            OnNotFound?.Invoke();            
+        private bool IsNotFound(IHtmlDocument document)
+        {
+            if(document.QuerySelectorAll("div.not-found").Length == 0) return false;
+            return true;
         }
     }
 }
--- WindowsApp/MainForm.Designer.cs
+++ WindowsApp/MainForm.Designer.cs
@@ -28,12 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.content = new System.Windows.Forms.TextBox();
+            this.textSearch = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // content
+            // 
+            this.content.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.content.Location = new System.Drawing.Point(22, 57);
+            this.content.Multiline = true;
+            this.content.Name = "content";
+            this.content.Size = new System.Drawing.Size(636, 429);
+            this.content.TabIndex = 0;
+            // 
+            // textSearch
+            // 
+            this.textSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textSearch.Location = new System.Drawing.Point(22, 30);
+            this.textSearch.Name = "textSearch";
+            this.textSearch.Size = new System.Drawing.Size(535, 20);
+            this.textSearch.TabIndex = 1;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSearch.Location = new System.Drawing.Point(565, 30);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(93, 20);
+            this.buttonSearch.TabIndex = 2;
+            this.buttonSearch.Text = "Поиск";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 498);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.textSearch);
+            this.Controls.Add(this.content);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox content;
+        private System.Windows.Forms.TextBox textSearch;
+        private System.Windows.Forms.Button buttonSearch;
     }
 }
 
